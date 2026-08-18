ae13ff677ad178604a6b1b91835c04c509cb48c7
mayankyadav19 <hatmario06@gmail.com>
2026-08-18 09:44:24 +0530

    initial commit

---
+++ javaScript/first.js
@@ -0,0 +1,11 @@
+// script: embeded language (needs another language to run)
+//<script> is a key identifier
+
+// document.write('we are code!');
+// document.write('Mayank');
+// alert('Hello there!'); //synchronous operation which blocks the main thread of the browser
+
+// console.log("hello"); //console is an object which represents the developers debug panel of browser, .log() is a method which logs data
+
+// const firstPara = document.getElementById('ad');
+// firstPara.innerHTML = `Hello world!`;
